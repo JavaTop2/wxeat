59fd6fb4249dac63b279383d2f92ae57eb1b31d3
yechao <1002164368@qq.com>
2018-05-16 11:55:45 +0800

    微信点餐

---
+++ wxeat/src/main/java/com/yc/wxeat/WxeatApplication.java
@@ -0,0 +1,12 @@
+package com.yc.wxeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxeatApplication.class, args);
+	}
+}
